14943139d91347d0e307cb484a0a660d9f013c51
JungByungCheon <star1431@naver.com>
2022-06-12 03:43:25 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/jbc_vue_practice/',
-  outputDir: 'docs'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/jbc_vue_practice/'
+  : '/',
+  outputDir: 'docs',
 
 })
